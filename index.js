563c8dd3bccaa651225b100a84c00f1e0f5c5765
Brandon <brandon.carnagli@gmail.com>
2023-08-12 01:29:42 -0300

    commit debbug

---
+++ index.js
@@ -0,0 +1,13 @@
+const fn = () => {
+    const miString = "Aguante boca"
+    let b = miString + "quiero la septima"
+    console.log(b)
+    }
+
+    fn()
+
+    const clickeado = () => {
+        let a = "boquita campeon"
+
+        console.log(a)
+        }
